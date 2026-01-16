efbfc9a817c1473c79400a547904be4b980ec80d
juan02pecam <152318402+juan02pecam@users.noreply.github.com>
2026-01-16 10:51:48 -0500

    new

---
+++ JavaScript/FreeCode/oraciones.js
@@ -0,0 +1,19 @@
+let adjective = "mysterious";
+let noun = "cat";
+let verb = "escaped";
+let place = "library";
+let adjective2 = "ancient";
+let noun2 = "secrets";
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+console.log(" ");
+
+adjective = "brave";
+noun = "dragon";
+verb ="to escape";
+place = "forest";
+adjective2 = "mysterious";
+noun2 = "treasure";
+let secondStory = "Once upon a time, there was a(n) " +  adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
---
+++ JavaScript/FreeCode/triviaBot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "IA FreeCode" ;
+const botLocation = "Colombia" ;
+const favoriteLanguage = "My favorite language is JavaScript" ;
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "A fun fact about " + favoriteLanguage + " was that it was created in just 10 days by Brendan Eich in 1995." ;
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " It was called Mocha, then LiveScript, and finally JavaScript, to take advantage of the popularity of Java at the time.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " Unlike other languages, undefined is not a reserved word in JavaScript.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");

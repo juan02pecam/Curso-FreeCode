942d45ede6f5799b208190a7345006aa3e7df619
juan02pecam <152318402+juan02pecam@users.noreply.github.com>
2026-02-02 12:44:43 -0500

    new

--- JavaScript/FreeCode/Botcon.js
+++ JavaScript/FreeCode/Botcon.js
@@ -20,12 +20,14 @@ console.log(nicknameIntroduction);
 
 bot = "awesomeTeacherBot";
 
-const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+const newNicknameGreeting =
+  "I love my nickname but I wish people would call me " + bot + ".";
 console.log(newNicknameGreeting);
 
 const favoriteSubject = "Computer Science";
 
-const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+const favoriteSubjectSentence =
+  "My favorite subject is " + favoriteSubject + ".";
 console.log(favoriteSubjectSentence);
 
 console.log("Well, it was nice to talk to you. Have a nice day!");

ba55b787015113c1e4b89a38a67db6b30a9d9dcf
juan02pecam <152318402+juan02pecam@users.noreply.github.com>
2026-03-06 11:27:12 -0500

    Create oneOne.js

---
+++ Aparte/Pruebas de GPT/oneOne.js
@@ -0,0 +1,10 @@
+const edad = 22
+let mensaje
+
+if (edad >= 19) {
+    mensaje = "Eres considerado un adulto por Juan"
+} else {
+    mensaje = "No eres considerado un adulto por Juan"
+}
+
+console.log(mensaje)

3012cc09d04332da1a93a70534420b6ffe13ee2f
juan02pecam <152318402+juan02pecam@users.noreply.github.com>
2026-03-13 16:42:28 -0500

    Update año.js

--- JavaScript/FreeCode/3. Funciones/año.js
+++ JavaScript/FreeCode/3. Funciones/año.js
@@ -3,9 +3,9 @@ const result = ""
 
 function isLeapYear(year) {
   if (year % 4 == 0 || year % 400 == 0 ) {
-    return result + `${year} is a leap year.`;
-  } else if (year % 100 == 0){
-    return result + `${year} is not a leap year.`;
+    console.log(result + `${year} is a leap year.`)
+  } else {
+    console.log(result + `${year} is not a leap year.`)
   }
             
 
@@ -13,7 +13,7 @@ function isLeapYear(year) {
 }
 
 
-isLeapYear (2004)
+isLeapYear (2024)
 isLeapYear (2000)
 isLeapYear (1900)
 console.log(result)
